384abb091c05276dd152c01c2cb5a9e1af15df73
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-05-02 16:19:23 -0500

    Update app.py

--- 03-Lesson-Plans/09-Lessons/2/Activities/04-Evr_Hello_Web/Unsolved/app.py
+++ 03-Lesson-Plans/09-Lessons/2/Activities/04-Evr_Hello_Web/Unsolved/app.py
@@ -1,16 +1,27 @@
 # 1. Import Flask
-
+from flask import Flask, jsonify
 
 # 2. Create an app
-
+app = Flask(__name__)
 
 # 3. Define index route
+@app.route("/")
+def home():
+    return "Hello, Web"
 
 
 # 4. Define the about route
+@app.route("/about")
+def normal():
+    return str('Opy' 'flask' 'Austin')
 
 
 # 5. Define the contact route
+@app.route("/contact")
+def jsonified_list():
+    return jsonify(ope@yahoo.me)
 
 
 # 6. Define main behavior
+if __name__ == "__main__":
+    app.run(debug=True)
